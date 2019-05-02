3850951631b53890226c7e906d7fe80ed83fc7d2
Sam Rang <srang2010@gmail.com>
2019-05-01 23:48:42 -0400

    wip

--- lab-manager/src/main/java/com/redhat/iot/web/AmqpReader.java
+++ lab-manager/src/main/java/com/redhat/iot/web/AmqpReader.java
@@ -12,10 +12,33 @@ import java.util.Arrays;
 
 @Component
 public class AmqpReader {
-    
+
     @JmsListener(destination = "user1")
     public void receiveMessage(Message message) throws Exception {
-        System.out.println("Received <" + message.getBody(String.class) + ">");
+        System.out.println("Received <" + message + ">");
+
+//        String[] payload = exchange.getIn().getBody(String.class).split(",");
+//        log.info(String.format("AMQP message (%s) consumed", Arrays.toString(payload)));
+//        Map<String, String> sensorData = new HashMap<>();
+//        sensorData.put("pumpId", payload[0]);
+//        sensorData.put("type", payload[1]);
+//        sensorData.put("timestamp", payload[2]);
+//        sensorData.put("value", payload[3]);
+//        switch (payload[1]) {
+//            case "temperature":
+//                sensorData.put("units", "F");
+//                break;
+//            case "vibration":
+//                sensorData.put("units", "g");
+//                break;
+//            case "throughput":
+//                sensorData.put("units", "bd");
+//                break;
+//            case "frequency":
+//                sensorData.put("units", "Hz");
+//                break;
+//        }
+
     }
 
 }
