40396992afd7834c1227cbc6ae264b539459f759
Sam Rang <srang2010@gmail.com>
2019-05-05 19:40:38 -0400

    end to end automation

--- bin/bootstrap-streams.sh
+++ bin/bootstrap-streams.sh
@@ -6,7 +6,7 @@ CMD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJ_DIR="${CMD_DIR}/.."
 
 NAMESPACE="kafka"
-APPLICATION_NAME="kafka"
+APPLICATION_NAME="iot-cluster"
 APPLICATION_CONTEXT_DIR="amq-streams"
 OLD_NS=$(oc projects | grep '*' | awk '{ print $2 }')
 oc project $NAMESPACE || oc new-project $NAMESPACE
@@ -21,6 +21,22 @@ if (oc get kafka/${APPLICATION_NAME} -n ${NAMESPACE} &>/dev/null); then
 fi
 oc apply -f ${PROJ_DIR}/message-bridge/templates/kafka-persistent.yml -n ${NAMESPACE}
 sleep 5s
+
+timer=0
+while [[ $(oc get deploy/strimzi-cluster-operator -ojsonpath='{.status.readyReplicas}' -n ${NAMESPACE}) != 1 ]] ; do
+    POD_STATUS=$(oc get pods -l=name=strimzi-cluster-operator -o=jsonpath='{ range.items[*] }{ .metadata.name }{ ", status: " }{ .status.phase }{ ", restarts: " }{ range .status.containerStatuses[*] }{ .restartCount }{ end} { "\n" }{ end }' -n ${NAMESPACE})
+    if [[ ${OLD_STATUS} != ${POD_STATUS} ]];  then
+        echo $POD_STATUS
+        OLD_STATUS=${POD_STATUS}
+    fi
+    RESTARTS=$(echo $POD_STATUS | awk '{ print $5 }')
+    sleep 1s
+    let timer=timer+1
+    if [[ ${timer} -gt 200 || ${RESTARTS} -gt 3 ]]; then
+        echo "${APPLICATION_NAME} failed to deploy. Timer: ${timer}, Restarts: ${RESTARTS}"
+        exit -1
+    fi
+done
 
 # wait for a deployments to come up
 timer=0
@@ -71,5 +87,7 @@ while [[ $(oc get deploy/${APPLICATION_NAME}-entity-operator -ojsonpath='{.statu
     fi
 done
 
-#oc project ${OLD_NS}
+oc apply -f ${PROJ_DIR}/message-bridge/templates/kafka-topic.yml -n ${NAMESPACE}
+
+oc project ${OLD_NS}
 
